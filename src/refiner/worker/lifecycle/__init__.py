d9f87109074ed2eb7485f3923bf43ed71ae9c118
Hynek Kydlicek <kydlicek.hynek@gmail.com>
2026-04-16 17:20:47 +0200

    restore finalized shard worker re-export

--- src/refiner/worker/lifecycle/__init__.py
+++ src/refiner/worker/lifecycle/__init__.py
@@ -1,5 +1,11 @@
+from refiner.platform.client.models import FinalizedShardWorker
 from refiner.worker.lifecycle.base import RuntimeLifecycle
 from refiner.worker.lifecycle.local import LocalRuntimeLifecycle
 from refiner.worker.lifecycle.platform import PlatformRuntimeLifecycle
 
-__all__ = ["LocalRuntimeLifecycle", "PlatformRuntimeLifecycle", "RuntimeLifecycle"]
+__all__ = [
+    "FinalizedShardWorker",
+    "LocalRuntimeLifecycle",
+    "PlatformRuntimeLifecycle",
+    "RuntimeLifecycle",
+]
