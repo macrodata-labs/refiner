fb1161ab16a49e1b15e53fd3d00a4f42d579bb0a
guipenedo <nostrumg@gmail.com>
2026-05-23 13:33:56 +0200

    Add Zarr writer

--- src/refiner/pipeline/sinks/__init__.py
+++ src/refiner/pipeline/sinks/__init__.py
@@ -2,6 +2,7 @@ from refiner.pipeline.sinks.base import BaseSink, NullSink
 from refiner.pipeline.sinks.jsonl import JsonlSink
 from refiner.pipeline.sinks.parquet import ParquetSink
 from refiner.pipeline.sinks.reducer import FileCleanupReducerSink, LeRobotMetaReduceSink
+from refiner.pipeline.sinks.zarr import ZarrSink
 
 __all__ = [
     "BaseSink",
@@ -10,4 +11,5 @@ __all__ = [
     "JsonlSink",
     "LeRobotMetaReduceSink",
     "ParquetSink",
+    "ZarrSink",
 ]
